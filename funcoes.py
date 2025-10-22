f116b5b29b6c5aa16d27b1ef1baa512d353f9632
Felipe Correia <felipecc9@al.insper.edu.br>
2025-10-21 23:34:01 -0300

    troquei nome do arquivo e fiz question3 - Felipe.C

--- funcoes.py
+++ funcoes.py
@@ -1,23 +1,9 @@
-def define_posicoes(linha, coluna, orientacao, tamanho):
-    posicoes = []
+def faz_jogada(tabuleiro, linha, coluna):
 
-    if orientacao == "vertical":
-        for i in range(tamanho):
-            posicoes.append([linha + i, coluna])
+    if tabuleiro[linha][coluna] == 1:
+        tabuleiro[linha][coluna] = 'X'
 
-    elif orientacao == "horizontal":
-        for i in range(tamanho):
-            posicoes.append([linha, coluna + i])
-
-    return posicoes
-
-
-def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
-    posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
-    
-    if nome_navio not in frota:
-        frota[nome_navio] = []
-    
-    frota[nome_navio].append(posicoes)
-    
-    return frota
+    elif tabuleiro[linha][coluna] == 0:
+        tabuleiro[linha][coluna] = '-'
+        
+    return tabuleiro
---
+++ funcoes2.py
@@ -0,0 +1,23 @@
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    posicoes = []
+
+    if orientacao == "vertical":
+        for i in range(tamanho):
+            posicoes.append([linha + i, coluna])
+
+    elif orientacao == "horizontal":
+        for i in range(tamanho):
+            posicoes.append([linha, coluna + i])
+
+    return posicoes
+
+
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
+    
+    if nome_navio not in frota:
+        frota[nome_navio] = []
+    
+    frota[nome_navio].append(posicoes)
+    
+    return frota
